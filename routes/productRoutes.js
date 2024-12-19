f63a4bf08cb958eade990b9da223907ace96b941
SAIYAM-SHUKLA <115667516+SAIYAM-SHUKLA@users.noreply.github.com>
2024-12-20 02:08:36 +0530

    readding auth

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -15,7 +15,7 @@ const router = express.Router();
 router.post("/", createProduct); // Add a product (seller only)
 router.get("/", getProducts); // Get all products
 router.get("/:id", getProductById); // Get a single product by ID
-router.put("/:id", protect, updateProduct); // Update a product (seller only)
-router.delete("/:id", protect, deleteProduct); // Delete a product (seller only)
+router.put("/:id",  updateProduct); // Update a product (seller only)
+router.delete("/:id",  deleteProduct); // Delete a product (seller only)
 
 module.exports = router;
