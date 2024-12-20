e1370f6e3d782e9c541c5cefa7b169dcb9b9e731
SAIYAM-SHUKLA <115667516+SAIYAM-SHUKLA@users.noreply.github.com>
2024-12-20 11:53:39 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 
 require('dotenv').config();
-const User = require("../models/User");
+const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
